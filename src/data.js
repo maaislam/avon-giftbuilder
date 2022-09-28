65983aad6455e4973ef12eb132fd725d39e4bfa3
arafatislam <maaislam@outlook.com>
2022-09-29 02:19:07 +0600

    mobile device css fix

--- src/data.js
+++ src/data.js
@@ -1,33 +1,35 @@
 export const pamperData = [
   {
-    name: 'GORGEOUS & GLAM',
+    name: 'Gorgeous & Glam Make-Up',
     currentPrice: 1200,
-    prevPrice: 2000,
+    prevPrice: 2100,
     image: 'gift-pamper1.png',
-    description: 'Build your own cosy night in set, unlock magical savings of upto 15% off.Everyone deserse a cosy night in!',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     btnText: 'Shop now',
     choice1: {
-      stepTitle: 'Choose the perfect lip shade and finish',
-      handles: ['avon-true-colour-lipstick', 'new-ultra-shimmer-lipstick'],
+      stepTitle: 'Choose the perfect lip finish and shade.',
+      handles: ['avon-true-ultra-satin-lipstick', 'avon-true-colour-lipstick', 'new-ultra-shimmer-lipstick'],
     },
     choice2: {
-      stepTitle: 'Choose to hightlight or bronze',
-      handles: ['true-colour-glow-bronzing-pearls', 'true-colour-illuminating-pearls'],
+      stepTitle: 'Choose a highlighter or bronzer.',
+      handles: ['avon-true-bronzing-pearls', 'true-colour-glow-bronzing-pearls', 'true-colour-illuminating-pearls'],
     },
     choice3: {
-      stepTitle: 'Choose a gorgeous make-up brush',
-      handles: ['all-over-face-brush', 'angled-blush-brush', 'kabuki-brush'],
+      stepTitle: 'Choose a gorgeous make-up brush.',
+      handles: ['all-over-face-brush-1', 'angled-blush-brush', 'kabuki-brush'],
     },
   },
   {
-    name: 'SKINCARE SAVIOURS',
-    currentPrice: 2600,
-    prevPrice: 4000,
+    name: 'Cleansing & Radiant Skincare',
+    currentPrice: 2800,
+    prevPrice: 4900,
     image: 'gift-pamper2.png',
-    description: 'Build your own cosy night in set, unlock magical savings of upto 15% off.Everyone deserse a cosy night in!',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     btnText: 'Shop now',
     choice1: {
-      stepTitle: 'Choose the perfect lip shade and finish',
+      stepTitle: 'Choose a refreshing cleanser.',
       handles: [
         'anew-micellar-water',
         'new-anew-purifying-jelly-cleanser-with-charcoal-extract-150ml',
@@ -35,7 +37,7 @@ export const pamperData = [
       ],
     },
     choice2: {
-      stepTitle: 'Choose to hightlight or bronze',
+      stepTitle: 'Choose the perfect moisturiser.',
       handles: [
         'anew-platinum-day-lifting-cream-spf25',
         'anew-ultimate-night-restoring-cream',
@@ -43,48 +45,104 @@ export const pamperData = [
       ],
     },
     choice3: {
-      stepTitle: 'Choose a gorgeous make-up brush',
+      stepTitle: 'Choose a skin-loving treatment.',
       handles: ['anew-radiance-maximising-serum', 'anew-anti-wrinkle-smoothing-serum', 'anew-protinol-power-serum'],
     },
   },
   {
-    name: 'RELAX & PAMPER',
-    currentPrice: 1400,
-    prevPrice: 2000,
+    name: 'Relaxing & Pampering',
+    currentPrice: 1200,
+    prevPrice: 2300,
     image: 'gift-pamper3.png',
-    description: 'Build your own cosy night in set, unlock magical savings of upto 15% off.Everyone deserse a cosy night in!',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     btnText: 'Shop now',
     choice1: {
-      stepTitle: 'Choose the perfect lip shade and finish',
+      stepTitle: 'Choose a relaxing face mask.',
       handles: ['anew-radiance-maximising-gold-mask', 'anew-lifting-silver-mask-75ml', 'anew-revitalising-copper-mask'],
     },
     choice2: {
-      stepTitle: 'Choose to hightlight or bronze',
-      handles: [],
+      stepTitle: 'Add the perfect head towel.',
+      handles: ['pink-wiggle-hair-towel'],
     },
     choice3: {
-      stepTitle: 'Choose a gorgeous make-up brush',
-      handles: [''],
+      stepTitle: 'Choose a nourishing hair treatment.',
+      handles: ['absolute-nourishment-treatment-hair-mask-150ml', 'reconstruction-treatment-hair-mask-150m'],
     },
   },
   {
-    name: 'COSY & CHILLED',
+    name: 'Cosy & Chilled',
     currentPrice: 1200,
-    prevPrice: 2000,
+    prevPrice: 1500,
     image: 'gift-pamper3.png',
-    description: 'Build your own cosy night in set, unlock magical savings of upto 15% off.Everyone deserse a cosy night in!',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     btnText: 'Shop now',
     choice1: {
-      stepTitle: 'Choose the perfect lip shade and finish',
-      handles: [],
+      stepTitle: 'Choose a relaxing treat.',
+      handles: [
+        'new-planet-spa-korean-charcoal-peel-off-face-mask-50ml',
+        'radiance-ritual-liquid-gold-peel-off-face-mask',
+        'new-planet-spa-heavenly-hydration-neck-chest-serum-50ml',
+      ],
     },
     choice2: {
-      stepTitle: 'Choose to hightlight or bronze',
-      handles: ['lavender-candle', 'white-jasmine-diffuser', 'new-sweet-rose-reed-diffuser-70ml'],
+      stepTitle: 'Choose a gorgeous fragrance.',
+      handles: ['new-vanilla-bean-diffuser-70ml', 'white-jasmine-diffuser', 'new-sweet-rose-reed-diffuser-70ml'],
     },
     choice3: {
-      stepTitle: 'Choose a gorgeous make-up brush',
+      stepTitle: 'Choose a bold colour.',
       handles: ['ultra-colour-60-second-express-nail-enamel'],
+    },
+  },
+  {
+    name: 'Calm & Unwinding',
+    currentPrice: 1200,
+    prevPrice: 1400,
+    image: 'gift-pamper3.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    btnText: 'Shop now',
+    choice1: {
+      stepTitle: 'Choose a scent-sational candle.',
+      handles: ['wild-berries-candle', 'sweet-rose-candle-120g', 'new-happy-uplifting-candle'],
+    },
+    choice2: {
+      stepTitle: 'Lorem ipsum dolor sit amet.',
+      handles: ['new-romantic-lamour-bubble-bath-500ml', 'white-lily-bubble-bath-500ml', 'lavender-bubble-bath-500ml'],
+    },
+    choice3: {
+      stepTitle: 'Lorem ipsum dolor sit amet.',
+      handles: [
+        'new-planet-spa-heavenly-hydration-moisturiser-200ml',
+        'planet-spa-radiance-ritual-golden-body-butter',
+        'new-planet-spa-energise-body-butter-200ml',
+      ],
+    },
+  },
+  {
+    name: 'FRESH & GROOMED',
+    currentPrice: 1500,
+    prevPrice: 1800,
+    image: 'gift-pamper3.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    btnText: 'Shop now',
+    choice1: {
+      stepTitle: 'Lorem ipsum dolor sit amet.',
+      handles: ['kris-washbag', 'musk-marine-eau-de-toilette-75ml', 'musk-intense-eau-de-toilette-75ml'],
+    },
+    choice2: {
+      stepTitle: 'Lorem ipsum dolor sit amet.',
+      handles: ['musk-air-eau-de-toilette-75ml'],
+    },
+    choice3: {
+      stepTitle: 'Lorem ipsum dolor sit amet.',
+      handles: [
+        'essentials-after-shave-balm-100ml',
+        'essentials-shave-gel-150ml',
+        'essentials-3-in-1-shampoo-conditioner-body-wash-200ml',
+      ],
     },
   },
 ];
